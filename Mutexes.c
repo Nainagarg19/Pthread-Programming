2f1a7c179ed1780d069f1d2f1657fe23ef03a1f4
Naina Garg <gargnaina19@gmail.com>
2021-04-28 14:59:15 +0530

    Update Mutexes.c

--- Mutexes.c
+++ Mutexes.c
@@ -8,10 +8,10 @@ void *mutex_testing(void *param)
 {
         int i;
         for(i = 0; i < 5; i++) {
-                pthread_mutex_lock(&myMutex;);
+                pthread_mutex_lock(&myMutex);
                 counter++;
                 printf("thread %d counter = %d\n", (int)param,  counter);
-                pthread_mutex_unlock(&myMutex;);
+                pthread_mutex_unlock(&myMutex);
         }
 }
 
@@ -19,10 +19,10 @@ int main()
 {
         int one = 1, two = 2, three = 3;
         pthread_t thread1, thread2, thread3;
-        pthread_mutex_init(&myMutex;,0);
-        pthread_create(&thread1;, 0, mutex_testing, (void*)one);
-        pthread_create(&thread2;, 0, mutex_testing, (void*)two);
-        pthread_create(&thread3;, 0, mutex_testing, (void*)three);
+        pthread_mutex_init(&myMutex,0);
+        pthread_create(&thread1, 0, mutex_testing, (void*)one);
+        pthread_create(&thread2, 0, mutex_testing, (void*)two);
+        pthread_create(&thread3, 0, mutex_testing, (void*)three);
         pthread_join(thread1, 0);
         pthread_join(thread2, 0);
         pthread_join(thread3, 0);
